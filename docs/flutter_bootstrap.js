76c003125702deff35c7da0db4a2b1b3d37e3074
LoKhyeHe <89993223+LoKhyeHe@users.noreply.github.com>
2026-06-01 15:52:13 +0800

    Rebuild docs for GitHub Pages deployment

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2994678041" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3702873349" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
